befcac96563885083f0e3840fdd6e02cc6142ff6
Антон <antonL1790@yandex.ru>
2022-01-11 18:57:49 +0300

    HomeWork#5 Fix

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-
         int[] arrOne = new int[]{1, 2, 3};
         System.out.println(Arrays.toString(arrOne));
         for (int i = 2; i >= 0; i--) {
